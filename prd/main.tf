25cf3de722b1cc9b8868603f0f214fbcd733519c
ciloholic <ciloholic0119@gmail.com>
2025-02-22 15:00:54 +0900

    add terraform backend remote

--- prd/main.tf
+++ prd/main.tf
@@ -1,1 +1,11 @@
+terraform {
+  backend "remote" {
+    organization = "ciloholic"
+
+    workspaces {
+      name = "test_prd"
+    }
+  }
+}
+
 resource "null_resource" "prd" {}
--- stg/main.tf
+++ stg/main.tf
@@ -1,1 +1,11 @@
+terraform {
+  backend "remote" {
+    organization = "ciloholic"
+
+    workspaces {
+      name = "test_stg"
+    }
+  }
+}
+
 resource "null_resource" "stg" {}

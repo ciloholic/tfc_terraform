62fd4944750315cadafb1efcf63182a8baaf66bc
ciloholic <ciloholic0119@gmail.com>
2025-02-26 21:35:58 +0900

    ip addr show

--- prd/main.tf
+++ prd/main.tf
@@ -11,3 +11,9 @@ terraform {
 resource "null_resource" "prd_1" {}
 
 resource "null_resource" "prd_2" {}
+
+resource "terraform_data" "command" {
+  provisioner "local-exec" {
+    command = "ip addr show"
+  }
+}
